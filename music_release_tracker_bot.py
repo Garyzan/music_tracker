870cd1e50cd619fccc1f40bf0742fd627f1d0d27
Garyzan <timo.meegdenburg@gmail.com>
2025-11-20 17:03:23 +0100

    basic data management

--- music_release_tracker_bot.py
+++ music_release_tracker_bot.py
@@ -1,50 +1,120 @@
 import asyncio
 import logging
 import sys
-from os import getenv
+import requests
+import os
+import json
+from datetime import date, timedelta
 from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-# Bot token can be obtained via https://t.me/BotFather
-load_dotenv()
-TOKEN = getenv("TOKEN")
+## Envorinment
 
-# All handlers should be attached to the Router (or Dispatcher)
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+ALLOWLIST = []
+try:
+    with open("allowlist", "r") as f:
+        ALLOWLIST = [int(user_id) for user_id in f.readlines()]
+except FileNotFoundError:
+    logging.log(logging.ERROR, "Allowlist file not found. Exiting....")
+    exit()
 
 dp = Dispatcher()
 
+
+## Command handlers
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    if not message.from_user.id in ALLOWLIST:
+        logging.log(logging.INFO, f"Message from blocked user {message.from_user.id}")
+        return
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
+@dp.message(Command("add"))
+@dp.message(Command("track"))
+async def add_tracking(message: Message) -> None:
+    if not message.from_user.id in ALLOWLIST:
+        logging.log(logging.INFO, f"Message from blocked user {message.from_user.id}")
+        return
+    
+    if(len(message.text.split(" ")) == 1):
+        await message.answer("Please include at least one MusicBrainz ID (multiple entries should be separated by spaces)")
+        return
+    
+    artist_ids = message.text.split(" ")[1:]
+    add_to_trackfile(message.from_user.id, artist_ids)
+    await message.answer(f"Tracking {', '.join(artist_ids)}!")
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
+@dp.message(Command("refresh"))
+async def refresh(message: Message) -> None:
+    if not message.from_user.id in ALLOWLIST:
+        logging.log(logging.INFO, f"Message from blocked user {message.from_user.id}")
+        return
+    
+    pass
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+@dp.message(Command("delete"))
+@dp.message(Command("remove"))
+async def remove_tracking(message: Message) -> None:
+    if not message.from_user.id in ALLOWLIST:
+        logging.log(logging.INFO, f"Message from blocked user {message.from_user.id}")
+        return
+    
+    if(len(message.text.split(" ")) == 1):
+        await message.answer("Please include an artists name or MusicBrainz ID")
+        return
+    
+    artist_id = message.text[7:]
+    add_to_trackfile(message.from_user.id, [artist_id])
+    await message.answer(f"Tracking {artist_id}!")
+
+
+
+## Helpers
+
+### TODO: implement x.x
+def get_releases_since(arid: str, date: str) -> list:
+    req_string = f"https://musicbrainz.org/ws/2/release?fmt=json&query=arid:{arid}%20AND%20date:[{date}%20TO%20*]"
+    #r = requests.get(req_string)
+    #print(f"\n{r.text}")
+
+def yesterday_str() -> str:
+    return (date.today() - timedelta(days=1)).strftime('%y-%m-%d')
+
+def add_to_trackfile(uid: int, arids: list[str]):
+    filepath = f"tracks/{uid}.json"
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as f:
+            tracking : dict = json.load(f)
+    else:
+        tracking = {}
+    for arid in arids:
+        tracking[arid] = yesterday_str()
+    with open(filepath, 'w') as f:
+        json.dump(tracking, f)
+
+def remove_from_trackfile(uid: int, arids: list[str]):
+    filepath = f"tracks/{uid}.json"
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as f:
+            tracking : dict = json.load(f)
+    else:
+        logging.log(logging.INFO, "Attempted to remove artist from empty trackfile")
+    for arid in arids:
+        tracking[arid] = yesterday_str()
+    with open(filepath, 'w') as f:
+        json.dump(tracking, f)
 
 
 async def main() -> None:
@@ -53,7 +123,6 @@ async def main() -> None:
 
     # And the run events dispatching
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
