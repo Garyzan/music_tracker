12b8509f978de9ad59a65cf247bf27483260c362
Garyzan <timo.meegdenburg@gmail.com>
2025-11-21 11:00:43 +0100

    change get_releases_since request to only check until today to not count unreleased stuff

--- mbdb_interface.py
+++ mbdb_interface.py
@@ -1,4 +1,6 @@
 import requests
+
+from datetime import date
 from ratelimit import limits, sleep_and_retry
 
 @sleep_and_retry
@@ -6,8 +8,9 @@ from ratelimit import limits, sleep_and_retry
 def mbdb_get(req_string):
     return requests.get(req_string)
 
-def get_releases_since(artist_id: str, date: str) -> requests.Response:
-    req_string = f"https://musicbrainz.org/ws/2/release?fmt=json&query=arid:{artist_id}%20AND%20date:[{date}%20TO%20*]"
+def get_releases_since(artist_id: str, startdate: str) -> requests.Response:
+    today = date.today().strftime('%Y-%m-%d')
+    req_string = f"https://musicbrainz.org/ws/2/release?fmt=json&query=arid:{artist_id}%20AND%20date:[{startdate}%20TO%20{today}]"
     return mbdb_get(req_string)
 
 def get_artist_name(artist_id: str) -> str:
